297e6a78ace5971fcf48d69a39abbb8f77e40fd2
benjamin <benjinoble@gmail.com>
2021-11-30 13:01:41 +1100

    Carousel

--- index2.js
+++ index2.js
@@ -1,32 +1,61 @@
-let bombsArray = [];
+const startingMinutes = 1;
+let time = startingMinutes * 60;
+
+const counter = document.querySelector(`#counter`);
+
+setInterval(updateCounter, 1000);
+
+function updateCounter() {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  seconds = seconds < 1 ? "0" + seconds : seconds;
+  counter.innerHTML = `${minutes}:${seconds}`;
+  time--;
+}
+
+/////////////////////////////////////////////
+
 
 const bombImage = document.querySelector('#bombBlast');
 
-const getBombs = async (url) => {
+const url = "http://localhost:3000/bombs/";
+
+const fetchBombImage = async (url) => {
   const response = await fetch(url);
-  const data = await response.json();
-  return data;
+  const Data = await response.json();
+  return Data;
 }
 
-const createHtml = (image) => {
-  return `<img src='${image}'/>`;
-}
-
-document.addEventListener('DOMContentLoaded', async () => {
-  const serverResponse = await getBombs('http://localhost:3000/bombs');
-  const serverData = serverResponse;
-
-  bombImage.innerHTML =+ createHtml(serverResponse);
-});
-
-const renderBomb = (bombObject) => {
-  const bombPic = bombBlast.querySelector("img");
-  bombPic.src = bombObject.image_url;
+const createBombImage = (imageItem) => {
+  return `<img src="${imageItem}" alt='Bomb Blast' />`;
 };
 
+document.addEventListener("DOMContentLoaded", async () => {
+  const serverResponse = await fetchBombImage('http://localhost:3000/bombs');
+  const serverData = serverResponse;
 
+  console.log(serverResponse);
 
+  serverData.forEach((imageItem) => {
+    bombImage.innerHTML += createBombImage(imageItem);
+  })
+});
 
+let myBomb = 0;
 
+bombCarousel(createBombImage);
 
-
+function bombCarousel() {
+  let i;
+  let x = bombBlast.querySelector("img");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myBomb++;
+  if (myBomb > x.length) {
+    myBomb = 1;
+  }
+  x[myBomb - 1].style.display = "block";
+  setTimeout(bombCarousel, 1000);
+}
